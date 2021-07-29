48b75c7db1d7e99f2e74ab21df32ade016cb6f57
Dmitriy Guskov <derpanter@gmail.com>
2021-07-29 22:34:17 +0300

    HomeWork 4.3

---
+++ hw432.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import socket
+import datetime
+import time
+import json
+import yaml
+
+
+i = 1
+srvlist = {'netology.loc':'333.333.333.333', 'devops.loc':'444.444.444.444', 'google.com':'555.555.555.555'}
+
+while 1 == 1:
+  for host in srvlist:
+    ip = socket.gethostbyname(host)
+    if ip != srvlist[host]:
+        print(str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")) +' [ERROR] ' + str(host) +' IP mistmatch: '+srvlist[host]+' '+ip)
+        srvlist[host] = ip
+
+        with open("/Users/derpanter/devops-netology/log_"+host+".json", 'w') as js:
+          json_data = json.dumps([{host : ip}])
+          js.write(json_data)
+        with open("/Users/derpanter/devops-netology/log_"+host+".yaml", 'w') as ym:
+          yaml_data= yaml.dump([{host : ip}])
+          ym.write(yaml_data)
+
+
+  i+=1
+# print(i)
+  time.sleep(5)
